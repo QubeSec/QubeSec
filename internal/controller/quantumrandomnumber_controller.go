966e8049724a432c1471e66677f65e63c92a8df5
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2023-11-28 19:32:08 +0000

    modified:   internal/controller/quantumrandomnumber_controller.go

--- internal/controller/quantumrandomnumber_controller.go
+++ internal/controller/quantumrandomnumber_controller.go
@@ -42,6 +42,7 @@ type QuantumRandomNumberReconciler struct {
 //+kubebuilder:rbac:groups=qubesec.io,resources=quantumrandomnumbers,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=qubesec.io,resources=quantumrandomnumbers/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=qubesec.io,resources=quantumrandomnumbers/finalizers,verbs=update
+//+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
